fef484c726d9a35ca75a897b4561946ed77a0005
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-11-10 13:49:33 -0300

    added autoloader

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+spl_autoload_register(function ($class) {
+    // Convert namespace separators to directory separators
+    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+    
+    // Base directory for your classes
+    $baseDir = __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR;
+    
+    // Build the full path
+    $file = $baseDir . $class . '.php';
+    
+    // If the file exists, require it
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
+
+
+
+?>
