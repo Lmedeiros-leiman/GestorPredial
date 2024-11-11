dce28e3494338665ac75b95571434d877e8ef1ed
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-11-11 04:57:15 -0300

    Autoloader is now its own file

--- index.php
+++ index.php
@@ -2,21 +2,7 @@
 
 use Core\Basics\Router\Router;
 
-spl_autoload_register(function ($class) {
-    // Convert namespace separators to directory separators
-    
-    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-    
-    // Base directory for your classes
-    $baseDir = __DIR__ . DIRECTORY_SEPARATOR;
-    
-    // Build the full path
-    $file = $baseDir . $class . '.php';
-    // If the file exists, require it
-    if (file_exists($file)) {
-        require_once $file;
-    }
-});
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'Core' . DIRECTORY_SEPARATOR . 'Basics' . DIRECTORY_SEPARATOR . 'Autoloader.php';
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'Configurables' . DIRECTORY_SEPARATOR . 'Configurables.php';
 
