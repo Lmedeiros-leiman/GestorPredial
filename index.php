224c41ceccd5eca5b0c15ef4dc6931925a648f4a
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-11-11 19:36:21 -0300

    index calls using directory_separator

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 
 use Core\Basics\Router\Router;
 
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'Core' . DIRECTORY_SEPARATOR . 'Basics' . DIRECTORY_SEPARATOR . 'Autoloader.php';
+require_once str_replace("/", DIRECTORY_SEPARATOR ,__DIR__ . "/Core/Basics/Autoloader.php");
 
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'Configurables' . DIRECTORY_SEPARATOR . 'Configurables.php';
+require_once str_replace("/",DIRECTORY_SEPARATOR, __DIR__ ."/Configurables/Configurables.php");
 
 $router = new Router();
 $router->parseRequest($_SERVER['REQUEST_URI']);
