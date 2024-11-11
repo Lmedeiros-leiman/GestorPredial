e5a9b0d881c6de5024a6bfd3b6c4eb0fcaec9dc4
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-11-11 04:56:45 -0300

    added migration console command

--- Core/Basics/Database/Migration/migrate.php
+++ Core/Basics/Database/Migration/migrate.php
@@ -1,14 +1,35 @@
 <?php
 namespace Core\Basics\Database\Migration;
 
-use Core\Basics\Database\Database;
+spl_autoload_register(function ($class) {
+    // Convert namespace separators to directory separators
+    
+    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+    
+    // Base directory for your classes
+    $baseDir = __DIR__ . DIRECTORY_SEPARATOR .'..'. DIRECTORY_SEPARATOR .'..'. DIRECTORY_SEPARATOR .'..'. DIRECTORY_SEPARATOR .'..'. DIRECTORY_SEPARATOR;
+    
+    // Build the full path
+    $file = $baseDir . $class . '.php';
+    // If the file exists, require it
+    echo("Loading : " . $file . "\n");
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
+
+require_once __DIR__ ."/..".DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."Configurables".DIRECTORY_SEPARATOR."Configurables.php";
+
+use Core\Basics\Database;
 use Core\Basics\Database\Migration\SchemaBuilder as MigrationSchemaBuilder;
 use Exception;
+
+
 
 abstract class Migration {
     protected $db;
 
-    public function __construct(Database $db) {
+    public function __construct(Database\Database $db) {
         $this->db = $db;
     }
     abstract public function up();
@@ -26,10 +47,10 @@ try {
     echo "Starting migrations...\n";
     
     // Initialize database and migrations table
-    $db = Database::checkDatabaseTables();
+    $db = Database\Database::checkDatabaseTables();
     
     // Get all migration files
-    $migrations = glob('Database/Migrations/*.php');
+    $migrations = glob( __DIR__ . '..'. DIRECTORY_SEPARATOR .'Tables'. DIRECTORY_SEPARATOR .'*.php' );
     
     foreach ($migrations as $migration) {
         // Get migration class name
