b3486797c8ed9aca69f32d0d167a66b51032d3ed
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-11-12 12:58:13 -0300

    simplified

--- Core/Basics/Database/CRUD.php
+++ Core/Basics/Database/CRUD.php
@@ -2,15 +2,17 @@
 // a wrapper class for the database class.
 // it simplifies data down to 4 commands.
 
-class CRUD {
+use Core\Basics\Database\Database;
+
+class CRUD extends Database {
+
     public static function create($table, $data) {}
     public static function read($table, $data) {}
     public static function update($table, $data) {}
     public static function delete($table, $data) {}
 
     public static function fetchAll($table, $data) {}
-    public static function fetchByCondition($table, $data
-    ) {}
+    public static function fetchByCondition($table, $data) {}
     public static function fetchByKey($table,$data,$key) {}
 }
 
