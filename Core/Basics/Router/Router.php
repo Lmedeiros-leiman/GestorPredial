36a4cfde0b440932bc86f806f1daf061365dbf28
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-11-12 20:42:15 -0300

    added new routes for when user logs in or registers.

--- Core/Basics/Router/Router.php
+++ Core/Basics/Router/Router.php
@@ -14,7 +14,10 @@ class Router {
                 '/' => ['HomeController', 'index'],
                 '/user/{id}' => ['UserController', 'show'] // Example route with a variable
             ],
-            'POST' => [],
+            'POST' => [
+                '/login' => ['LoginController', 'login'],
+                '/register' => ['RegisterController', 'createAccount']
+            ],
             'DELETE' => [],
             'PUT' => [],
             'PATCH' => []
@@ -23,21 +26,22 @@ class Router {
 
     public function ParseRequest($url) {
         $method = $_SERVER['REQUEST_METHOD'];
+        
+        $url = $_SERVER["REDIRECT_URL"];
+        if (!isset($_SESSION["auth"])) {
+            if ($url != "/login" && $url != "/register") {
+                header("Location: /login");
+        } }
 
-        // Check if the URL matches any defined routes
-        foreach ($this->routes[$method] as $route => $action) {
-            // Use regex to match the route and extract parameters
-            $pattern = preg_replace('/\{(\w+)\}/', '(\w+)', $route); // Convert {id} to (\\w+)
-            if (preg_match("#^$pattern$#", $url, $matches)) {
-                array_shift($matches); // Remove the full match from the array
-                $controller = "Core\\Controllers\\" . $action[0];
-                $method = $action[1];
 
-                $controllerInstance = new $controller();
-                echo $controllerInstance->$method(...$matches); // Pass parameters to the method
-                return;
-            }
-        }
+        if (isset($this->routes[$method][$url])) {
+            $action = $this->routes[$method][$url];
+            $controller = "Core\\Controllers\\" . $action[0];
+            $method = $action[1];
+            $controllerInstance = new $controller();
+            $controllerInstance->$method();
+            return;
+        } 
 
         // If no route matched, load a public file or 404 page
         $fileURL = str_replace("/", DIRECTORY_SEPARATOR, "./public".$url);
