77db84ffa9a954d5e1d5e6d288d9da1462c2614a
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-11-12 13:01:51 -0300

    now working with the new controler base place

--- Core/Controllers/HomeController.php
+++ Core/Controllers/HomeController.php
@@ -2,17 +2,15 @@
 
 namespace Core\Controllers;
 
-use Core\Basics\BaseClasses\Controller;
 
 class HomeController extends Controller {
     public function index() {
-        $data = [];
-        return $this->render('index', $data);
+        return $this->render('index', $this->data);
     }
 
     public function notFound() {
-        $data = [];
-        return $this->render('404', $data);
+        
+        return $this->render('404', $this->data);
     }
 }
 
--- Core/Controllers/UserController.php
+++ Core/Controllers/UserController.php
@@ -2,11 +2,12 @@
 
 namespace Core\Controllers;
 
-use Core\Basics\BaseClasses\Controller;
 
 class UserController extends Controller {
     public function show($id) {
         // Logic to show user details based on the ID
+        
+
         return "Showing user with ID: " . htmlspecialchars($id);
     }
 }
