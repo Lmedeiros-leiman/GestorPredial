751e9df011ae0c646092f7c9d091f3164a52626f
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-11-12 12:56:21 -0300

    simplified function to a constant

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <?php
 use Core\Basics\Router\Router;
 
-function ViewRoute($file) {
-    $baseDir = ROOT_DIR . "Core/Views/";
-    return $baseDir . $file;
-}
-
 // declare some constant values.
 define("ROOT_DIR",__DIR__ . DIRECTORY_SEPARATOR);
+define("VIEW_DIR",ROOT_DIR. "Core/Views/");
 
 // calls autoLoader file
 require_once str_replace('/', DIRECTORY_SEPARATOR, ROOT_DIR."Core/Basics/Autoloader.php");
