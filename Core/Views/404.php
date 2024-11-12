f6bfcbd92dc9306ac8f31b070a4e6ad06e488381
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-11-12 12:57:14 -0300

    now uses the new function

--- Core/Views/404.php
+++ Core/Views/404.php
@@ -1,10 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<?php require_once VIEW_DIR . "Shared/Head.hphp"; ?>
 <body>
     Not found page.
 </body>
--- Core/Views/index.php
+++ Core/Views/index.php
@@ -1,10 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-</head>
+<?php require_once VIEW_DIR . "Shared/Head.hphp"; ?>
 <body>
     This is the default view index file!
 </body>
