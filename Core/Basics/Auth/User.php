73ba3341f77e86f964c5e14840bf561878dd1887
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-11-13 21:50:36 -0300

    we can now create users.

--- Core/Basics/Auth/User.php
+++ Core/Basics/Auth/User.php
@@ -1,5 +1,8 @@
 <?php 
 // Wrapper class for user data.
+namespace Core\Basics\Auth;
+
+use Core\Basics\Database\CRUD;
 
 class User {
     public readonly string $id;
@@ -7,8 +10,43 @@ class User {
     public readonly string $password;
     public readonly array $permissions;
 
+    
+    
 
-    public static function findByEmail() { return null;}
+    public static function create(string $email, string $password, array $permissions) {
+        
+    }
+    
+    public static function CreateAccount($email, $password, $permissions) {
+        $Crud = new CRUD();
+        
+        $PasswordId = $Crud->Create('secrets', [ 
+            'salt' => '',
+            'value' => password_hash($password, PASSWORD_BCRYPT) ]);
+
+        $Crud->create('users', [ 
+            'email' => $email, 
+            'password' => $PasswordId,
+            'confirmed_email' => false ]);
+        
+        return User::findByEmail($email);
+    }
+    public static function UpdateUser(User $originalUser, User $newUser){
+
+    }
+    public function DeleteUser(User $user){
+
+    }
+
+    public static function findByEmail($email) { 
+        $Crud = new CRUD();
+        $result = $Crud->read('users', [ 'email' => $email ]);
+        if (empty($result)) {
+            return false;
+        } else {
+            return $result[0];
+        }
+    }
 }
 
 ?>
