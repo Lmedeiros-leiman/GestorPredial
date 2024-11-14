3ff4be16964bd7d30ba83ec5dcdf48b64a6c007e
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-11-13 21:52:58 -0300

    users can now register into the app.

--- Core/Controllers/RegisterController.php
+++ Core/Controllers/RegisterController.php
@@ -2,13 +2,34 @@
 
 namespace Core\Controllers;
 
+use Core\Basics\Auth\User;
+use Core\Basics\Database\CRUD;
 
 class RegisterController extends Controller {
-    public function index() {
+  
+  protected readonly CRUD $CRUD;
+  public function __construct() {
+    parent::__construct();
+    $this->CRUD = new CRUD();
+  }
+  public function index() {
+      
       return $this->render('register', $this->data);
     }
 
-    
+    public function register() {
+      if (User::findByEmail($_POST['email'])) {
+        $_SESSION['message'] = 'danger#Email already exists';
+        header('Location: /register');
+        exit();
+      }
+
+      $newUser = User::CreateAccount($_POST['email'], $_POST['password'], []);
+      $_SESSION['user'] = $newUser->email;
+      
+      header('Location: /');
+      exit();
+    }
 }
 
 ?>
